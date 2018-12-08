904017254fe7e5751173012551ab917c60c21362
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2018-12-08 21:25:48 +0700

    function naming conventions

---
+++ naming/naming_test.go
@@ -0,0 +1,29 @@
+package naming
+
+import (
+	"testing"
+)
+
+func TestDog(t *testing.T) {
+
+}
+
+func TestSpeak(t *testing.T) {
+
+}
+
+func TestDog_Bark(t *testing.T) {
+
+}
+
+func TestDog_Bark_muzzled(t *testing.T) {
+
+}
+
+func TestDog_Bark_unmuzzled(t *testing.T) {
+
+}
+
+func TestSpeak_spanish(t *testing.T) {
+
+}
