ce867f085b1d915862377f9c25ee3e1e4c21ad9e
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2018-12-13 21:46:02 +0700

    finish way to signal test failure

--- signal/signal_test.go
+++ signal/signal_test.go
@@ -1,9 +1,50 @@
 package signal
 
-import "testing"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestHandler(t *testing.T) {
-	t.Fatal("this is error")
-	t.Logf("this is a log with : %v", "ok")
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest(http.MethodGet, "", nil)
+	if err != nil {
+	}
+
+	Handler(w, r)
+
+	resp := w.Result()
+	if resp.StatusCode != 200 {
+		t.Fatalf("Handler() status=%d; want %d", resp.StatusCode, 200)
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+	if contentType != "application/json" {
+		t.Errorf("Handler() Content-Type = %q; want %q", contentType, "application/json")
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatalf("ioutil.ReadAll(resp.Body) err : %p", err)
+	}
+
+	var p Person
+	err = json.Unmarshal(data, &p)
+	if err != nil {
+		t.Fatalf("json.Unmarshal(resp.Body) err : %p", err)
+	}
+
+	wantAge := 30
+	if p.Age != wantAge {
+		t.Errorf("person.Age = %d; want %d", p.Age, wantAge)
+	}
+
+	wantName := "John Doe"
+	if p.Name != wantName {
+		t.Errorf("person.Name = %s; want %s", p.Name, wantName)
+	}
 
 }
