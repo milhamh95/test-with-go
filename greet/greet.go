caa6c826b55448521d16c3857c6f2dea52bbb0fb
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2018-12-15 12:49:45 +0700

    unordered example output

--- greet/greet.go
+++ greet/greet.go
@@ -17,7 +17,7 @@ func Hello(name string) (string, error) {
 func Page(checkIns map[string]bool) {
 	for name, checkIn := range checkIns {
 		if !checkIn {
-			fmt.Printf("Paging %s: please see the front desk to check in", name)
+			fmt.Printf("Paging %s; please see the front desk to check in.\n", name)
 		}
 	}
 }
--- greet/greet_test.go
+++ greet/greet_test.go
@@ -1,7 +1,5 @@
 package greet
 
-import "fmt"
-
 // func ExampleHello() {
 // 	greeting, err := Hello("Jon")
 // 	if err != nil {
@@ -35,13 +33,30 @@ import "fmt"
 // 	// Hello, Jon
 // }
 
-func ExampleHello_spanish() {
-	greeting, err := Hello("Jon")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(greeting)
+// func ExampleHello_spanish() {
+// 	greeting, err := Hello("Jon")
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Println(greeting)
 
-	// Output:
-	// Hello, Jon
+// 	// Output:
+// 	// Hello, Jon
+// }
+
+func ExamplePage() {
+	checkIns := map[string]bool{
+		"Bob":   true,
+		"Alice": false,
+		"Eve":   false,
+		"Janet": true,
+		"Susan": true,
+		"John":  false,
+	}
+	Page(checkIns)
+
+	// Unordered output:
+	// Paging Alice; please see the front desk to check in.
+	// Paging Eve; please see the front desk to check in.
+	// Paging John; please see the front desk to check in.
 }
