007957e9a3a4a95a00687eac3ac07b36f85586b0
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2018-12-15 14:30:58 +0700

    complex examples

--- section-5-example-test-cases/img/img_test.go
+++ section-5-example-test-cases/img/img_test.go
@@ -1,15 +1,43 @@
 package img
 
-import "testing"
+import (
+	"fmt"
+	"io"
+)
 
-func TestDecode(t *testing.T) {
+// func TestDecode(t *testing.T) {
 
-}
+// }
 
-func TestCrop(t *testing.T) {
+// func TestCrop(t *testing.T) {
 
-}
+// }
 
-func TestEncode(t *testing.T) {
+// func TestEncode(t *testing.T) {
 
+// }
+
+func ExampleCrop() {
+	var r io.Reader
+	img, err := Decode(r)
+	if err != nil {
+		panic(err)
+	}
+
+	err = Crop(img, 0, 0, 20, 20)
+	if err != nil {
+		panic(err)
+	}
+
+	var w io.Writer
+
+	err = Encode(img, w)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("See out.jpg for the cropped image.")
+
+	// Output:
+	// See out.jpg for the cropped image.
 }
