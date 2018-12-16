79ab0bc71b24eae151da72734532f86dbb098060
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2018-12-16 14:20:17 +0700

    generate table driven test code

--- section-6-testing-multiple-case/underscore/underscore_test.go
+++ section-6-testing-multiple-case/underscore/underscore_test.go
@@ -7,26 +7,49 @@ type testCase struct {
 	want string
 }
 
-func TestCamel(t *testing.T) {
-	testCases := []testCase{
-		{
-			"thisIsACamelCaseString",
-			"this_is_a_camel_case_string",
-		},
-		{
-			"with a space",
-			"with a space",
-		},
-		{
-			"endsWithA",
-			"ends_with_a",
-		},
-	}
-	for _, tt := range testCases {
-		got := Camel(tt.arg)
-		if got != tt.want {
-			t.Errorf("Camel(%q) = %q; want %q", tt.arg, got, tt.want)
-		}
-	}
+// func TestCamel(t *testing.T) {
+// 	testCases := []testCase{
+// 		{
+// 			"thisIsACamelCaseString",
+// 			"this_is_a_camel_case_string",
+// 		},
+// 		{
+// 			"with a space",
+// 			"with a space",
+// 		},
+// 		{
+// 			"endsWithA",
+// 			"ends_with_a",
+// 		},
+// 	}
+// 	for _, tt := range testCases {
+// 		got := Camel(tt.arg)
+// 		if got != tt.want {
+// 			t.Errorf("Camel(%q) = %q; want %q", tt.arg, got, tt.want)
+// 		}
+// 	}
 
+// }
+
+func TestCamel(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{"simple", args{"thisIsACamelCaseString"}, "this_is_a_camel_case_string"},
+		{"spaces", args{"with a space"}, "with a space"},
+		{"ends with capital", args{"endsWithA"}, "ends_with_a"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Camel(tt.args.str); got != tt.want {
+				t.Errorf("Camel(%v) = %v, want %v", tt.args.str, got, tt.want)
+			}
+		})
+	}
 }
