44d610b2fe68a2d3477fdc1463a1fc64b391e2ba
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-01-20 13:50:35 +0700

    setup and teardown with parallel subtest

--- section-7-parallel-tests/parallel/parallel_test.go
+++ section-7-parallel-tests/parallel/parallel_test.go
@@ -1,26 +1,36 @@
 package parallel
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
-func TestSomething(t *testing.T) {
-	t.Parallel()
-}
+// func TestSomething(t *testing.T) {
+// 	t.Parallel()
+// }
 
-func TestA(t *testing.T) {
-	t.Parallel()
-}
+// func TestA(t *testing.T) {
+// 	t.Parallel()
+// }
 
 func TestB(t *testing.T) {
-	t.Parallel()
-	t.Run("sub 1", func(t *testing.T) {
-		t.Parallel()
-		time.Sleep(time.Second)
+	fmt.Println("setup")
+	defer fmt.Println("deferred teardown")
+
+	// make sure test run first
+	t.Run("group", func(t *testing.T) {
+		t.Run("sub 1", func(t *testing.T) {
+			t.Parallel()
+			time.Sleep(time.Second)
+			fmt.Println("sub1 done")
+		})
+		t.Run("sub 2", func(t *testing.T) {
+			t.Parallel()
+			time.Sleep(time.Second)
+			fmt.Println("sub2 done")
+		})
 	})
-	t.Run("sub 2", func(t *testing.T) {
-		t.Parallel()
-		time.Sleep(time.Second * 2)
-	})
+
+	fmt.Println("teardown")
 }
