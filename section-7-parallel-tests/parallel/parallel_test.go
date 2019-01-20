852cacbfbc75edcad7afaeebeae322ca5748b014
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-01-20 14:21:24 +0700

    closure parallel test

--- section-7-parallel-tests/parallel/parallel_test.go
+++ section-7-parallel-tests/parallel/parallel_test.go
@@ -3,7 +3,6 @@ package parallel
 import (
 	"fmt"
 	"testing"
-	"time"
 )
 
 // func TestSomething(t *testing.T) {
@@ -14,23 +13,46 @@ import (
 // 	t.Parallel()
 // }
 
-func TestB(t *testing.T) {
-	fmt.Println("setup")
-	defer fmt.Println("deferred teardown")
+// func TestB(t *testing.T) {
+// 	fmt.Println("setup")
+// 	defer fmt.Println("deferred teardown")
 
-	// make sure test run first
-	t.Run("group", func(t *testing.T) {
-		t.Run("sub 1", func(t *testing.T) {
-			t.Parallel()
-			time.Sleep(time.Second)
-			fmt.Println("sub1 done")
-		})
-		t.Run("sub 2", func(t *testing.T) {
-			t.Parallel()
-			time.Sleep(time.Second)
-			fmt.Println("sub2 done")
-		})
-	})
+// 	// make sure test run first
+// 	t.Run("group", func(t *testing.T) {
+// 		t.Run("sub 1", func(t *testing.T) {
+// 			t.Parallel()
+// 			time.Sleep(time.Second)
+// 			fmt.Println("sub1 done")
+// 		})
+// 		t.Run("sub 2", func(t *testing.T) {
+// 			t.Parallel()
+// 			time.Sleep(time.Second)
+// 			fmt.Println("sub2 done")
+// 		})
+// 	})
 
-	fmt.Println("teardown")
+// 	fmt.Println("teardown")
+// }
+
+type TestCase struct {
+	arg  int
+	want int
+}
+
+func TestGotcha(t *testing.T) {
+	testCases := []TestCase{
+		{2, 5},
+		{3, 9},
+		{4, 16},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(fmt.Sprintf("arg=%d", tc.arg), func(t *testing.T) {
+			t.Parallel()
+			t.Logf("Testing with arg = %d, want = %d", tc.arg, tc.want)
+			if tc.arg*tc.arg != tc.want {
+				t.Errorf("%d^2 != %d", tc.arg, tc.want)
+			}
+		})
+	}
 }
