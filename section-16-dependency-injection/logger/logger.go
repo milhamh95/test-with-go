b28190d33aa3631f351055c438dda05acdbe7cbd
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-02-21 11:56:06 +0700

    di enables implementation agnostic code

--- section-16-dependency-injection/logger/logger.go
+++ section-16-dependency-injection/logger/logger.go
@@ -21,6 +21,45 @@ func DemoV2(logger *log.Logger) {
 	}
 }
 
+// logger := log.New(...)
+// DemoV$(log.Println)
+func DemoV3(logFn func(...interface{})) {
+	err := doTheThing()
+	if err != nil {
+		logFn("error in doTheThing():", err)
+	}
+}
+
+type Logger interface {
+	Println(...interface{})
+	Printf(string, ...interface{})
+}
+
+// logger := log.new(...)
+// Demov4(logger)
+func DemoV4(logger Logger) {
+	err := doTheThing()
+	if err != nil {
+		logger.Println("error in doTheThing():", err)
+		logger.Printf("error : %s\n", err)
+	}
+}
+
+type Thing struct {
+	Logger interface {
+		Println(...interface{})
+		Printf(string, ...interface{})
+	}
+}
+
+func (t Thing) DemoV5() {
+	err := doTheThing()
+	if err != nil {
+		t.Logger.Println("error in doTheThing(): ", err)
+		t.Logger.Printf("error : %s\n", err)
+	}
+}
+
 func doTheThing() error {
 	return errors.New("error opening file: abc.txt")
 }
