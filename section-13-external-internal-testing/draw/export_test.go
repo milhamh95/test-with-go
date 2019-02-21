8b283b20f006804464a4fac74b392198e4c3750c
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-02-21 09:48:03 +0700

    export alias

---
+++ section-13-external-internal-testing/draw/export_test.go
@@ -0,0 +1,9 @@
+package draw
+
+var Fib = fib
+
+var A = a
+
+type Dog = dog
+
+var Info = info
--- section-13-external-internal-testing/draw/fib_gradient_test.go
+++ section-13-external-internal-testing/draw/fib_gradient_test.go
@@ -1,6 +1,7 @@
 package draw_test
 
 import (
+	"fmt"
 	"image/draw"
 	td "test-with-go/section-13-external-internal-testing/draw"
 	"testing"
@@ -10,3 +11,16 @@ func TestFibGradient(t *testing.T) {
 	var im draw.Image
 	td.FibGradient(im)
 }
+
+func TestFibFunc(t *testing.T) {
+	fmt.Println(td.A)
+	got := td.Fib(2)
+	if got != 1 {
+		t.Errorf("Fib(2) = %d, want 1", got)
+	}
+}
+
+// func TestInfo(t *testing.T) {
+// 	d := td.Dog{}
+// 	td.Info(d)
+// }
