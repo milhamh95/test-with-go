a2055e4965e4817d024d8a637eaea9f2e0be2876
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-02-27 21:32:06 +0700

    support more types

--- project-1-form/fields.go
+++ project-1-form/fields.go
@@ -4,15 +4,32 @@ import (
 	"reflect"
 )
 
-func fields(strct interface{}) []field {
-	rv := reflect.ValueOf(strct)
-
-	if rv.Kind() == reflect.Ptr {
-		rv = rv.Elem()
+func valueOf(v interface{}) reflect.Value {
+	var rv reflect.Value
+	switch value := v.(type) {
+	case reflect.Value:
+		rv = value
+	default:
+		rv = reflect.ValueOf(v)
 	}
 
+	// with any pointers we really want to just work with their underlying
+	// type
+	if rv.Kind() == reflect.Ptr {
+		// The underlying type is pretty useless if it is nil, so we need to
+		// instantiate a new copy of whatever that is before using it
+		if rv.IsNil() {
+			rv = reflect.New(rv.Type().Elem())
+		}
+		rv = rv.Elem()
+	}
+	return rv
+}
+
+func fields(strct interface{}) []field {
+	rv := valueOf(strct)
 	if rv.Kind() != reflect.Struct {
-		panic("form: invalid value only struct are supported")
+		panic("form: invalid value; only structs are supported")
 	}
 
 	t := rv.Type()
@@ -21,9 +38,17 @@ func fields(strct interface{}) []field {
 
 	for i := 0; i < t.NumField(); i++ {
 		tf := t.Field(i)
-		rvf := rv.Field(i)
-
+		rvf := valueOf(rv.Field(i))
 		if !rvf.CanInterface() {
+			continue
+		}
+
+		if rvf.Kind() == reflect.Struct {
+			nestedFields := fields(rvf.Interface())
+			for i, nf := range nestedFields {
+				nestedFields[i].Name = tf.Name + "." + nf.Name
+			}
+			ret = append(ret, nestedFields...)
 			continue
 		}
 
--- project-1-form/fields_test.go
+++ project-1-form/fields_test.go
@@ -7,6 +7,11 @@ import (
 )
 
 func TestFields(t *testing.T) {
+	var nilStructPtr *struct {
+		Name string
+		Age  int
+	}
+
 	tests := map[string]struct {
 		strct interface{}
 		want  []field
@@ -132,7 +137,7 @@ func TestFields(t *testing.T) {
 				},
 			},
 		},
-		"Pointer should be supported": {
+		"Pointer to struct should be supported": {
 			strct: &struct {
 				Name string
 				Age  int
@@ -154,6 +159,180 @@ func TestFields(t *testing.T) {
 					Type:        "text",
 					Placeholder: "Age",
 					Value:       123,
+				},
+			},
+		},
+		"Nil pointers with a struct type should be supported": {
+			strct: nilStructPtr,
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "",
+				},
+				{
+					Label:       "Age",
+					Name:        "Age",
+					Type:        "text",
+					Placeholder: "Age",
+					Value:       0,
+				},
+			},
+		},
+		"Pointer fields should be supported": {
+			strct: struct {
+				Name *string
+				Age  *int
+			}{},
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "",
+				},
+				{
+					Label:       "Age",
+					Name:        "Age",
+					Type:        "text",
+					Placeholder: "Age",
+					Value:       0,
+				},
+			},
+		},
+		"Nested structs should be supported": {
+			strct: struct {
+				Name    string
+				Address struct {
+					Street string
+					Zip    int
+				}
+			}{
+				Name: "Jon Calhoun",
+				Address: struct {
+					Street string
+					Zip    int
+				}{
+					Street: "123 Fake St",
+					Zip:    90210,
+				},
+			},
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "Jon Calhoun",
+				},
+				{
+					Label:       "Street",
+					Name:        "Address.Street",
+					Type:        "text",
+					Placeholder: "Street",
+					Value:       "123 Fake St",
+				},
+				{
+					Label:       "Zip",
+					Name:        "Address.Zip",
+					Type:        "text",
+					Placeholder: "Zip",
+					Value:       90210,
+				},
+			},
+		},
+		"Doubly nested structs should be supported": {
+			strct: struct {
+				A struct {
+					B struct {
+						C1 string
+						C2 int
+					}
+				}
+			}{
+				A: struct {
+					B struct {
+						C1 string
+						C2 int
+					}
+				}{
+					B: struct {
+						C1 string
+						C2 int
+					}{
+						C1: "C1-value",
+						C2: 123,
+					},
+				},
+			},
+			want: []field{
+				{
+					Label:       "C1",
+					Name:        "A.B.C1",
+					Type:        "text",
+					Placeholder: "C1",
+					Value:       "C1-value",
+				},
+				{
+					Label:       "C2",
+					Name:        "A.B.C2",
+					Type:        "text",
+					Placeholder: "C2",
+					Value:       123,
+				},
+			},
+		},
+		"Nested pointer structs should be supported": {
+			strct: struct {
+				Name    string
+				Address *struct {
+					Street string
+					Zip    int
+				}
+				ContactCard *struct {
+					Phone string
+				}
+			}{
+				Name: "Jon Calhoun",
+				Address: &struct {
+					Street string
+					Zip    int
+				}{
+					Street: "123 Fake St",
+					Zip:    90210,
+				},
+			},
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "Jon Calhoun",
+				},
+				{
+					Label:       "Street",
+					Name:        "Address.Street",
+					Type:        "text",
+					Placeholder: "Street",
+					Value:       "123 Fake St",
+				},
+				{
+					Label:       "Zip",
+					Name:        "Address.Zip",
+					Type:        "text",
+					Placeholder: "Zip",
+					Value:       90210,
+				},
+				{
+					Label:       "Phone",
+					Name:        "ContactCard.Phone",
+					Type:        "text",
+					Placeholder: "Phone",
+					Value:       "",
 				},
 			},
 		},
