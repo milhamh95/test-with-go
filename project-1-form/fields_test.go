653b0ffe4564b7ab06bd942d09bcd43b7fbc48d9
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-02-27 16:50:39 +0700

    unexported values

--- project-1-form/fields_test.go
+++ project-1-form/fields_test.go
@@ -1,17 +1,16 @@
 package form
 
 import (
-	"fmt"
 	"reflect"
 	"testing"
 )
 
 func TestFields(t *testing.T) {
-	tests := []struct {
+	tests := map[string]struct {
 		strct interface{}
 		want  []field
 	}{
-		{
+		"Simplest use case": {
 			// value in field
 			strct: struct {
 				Name string
@@ -26,8 +25,52 @@ func TestFields(t *testing.T) {
 				},
 			},
 		},
-		{
+		"Field names should be determined from the struct": {
 			// value in field
+			strct: struct {
+				FullName string
+			}{},
+			want: []field{
+				{
+					Label:       "FullName",
+					Name:        "FullName",
+					Type:        "text",
+					Placeholder: "FullName",
+					Value:       "",
+				},
+			},
+		},
+		"Multiple fields should be supported": {
+			strct: struct {
+				Name  string
+				Email string
+				Age   int
+			}{},
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "",
+				},
+				{
+					Label:       "Email",
+					Name:        "Email",
+					Type:        "text",
+					Placeholder: "Email",
+					Value:       "",
+				},
+				{
+					Label:       "Age",
+					Name:        "Age",
+					Type:        "text",
+					Placeholder: "Age",
+					Value:       0,
+				},
+			},
+		},
+		"Values should be parsed": {
 			strct: struct {
 				Name  string
 				Email string
@@ -61,9 +104,36 @@ func TestFields(t *testing.T) {
 				},
 			},
 		},
+		"Unexported fields should be skipped": {
+			strct: struct {
+				Name  string
+				email string
+				Age   int
+			}{
+				Name:  "Jon Calhoun",
+				email: "jon@calhoun.io",
+				Age:   123,
+			},
+			want: []field{
+				{
+					Label:       "Name",
+					Name:        "Name",
+					Type:        "text",
+					Placeholder: "Name",
+					Value:       "Jon Calhoun",
+				},
+				{
+					Label:       "Age",
+					Name:        "Age",
+					Type:        "text",
+					Placeholder: "Age",
+					Value:       123,
+				},
+			},
+		},
 	}
-	for _, tc := range tests {
-		t.Run(fmt.Sprintf("%T", tc.strct), func(t *testing.T) {
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
 			got := fields(tc.strct)
 			if !reflect.DeepEqual(got, tc.want) {
 				t.Errorf("fields() = %v; want %v", got, tc.want)
@@ -72,75 +142,75 @@ func TestFields(t *testing.T) {
 	}
 }
 
-func TestFields_labels(t *testing.T) {
-	hasLabels := func(labels ...string) func(*testing.T, []field) {
-		return func(t *testing.T, fields []field) {
-			if len(fields) != len(labels) {
-				t.Fatalf("fields() len = %d; want %d", len(fields), len(labels))
-			}
+// func TestFields_labels(t *testing.T) {
+// 	hasLabels := func(labels ...string) func(*testing.T, []field) {
+// 		return func(t *testing.T, fields []field) {
+// 			if len(fields) != len(labels) {
+// 				t.Fatalf("fields() len = %d; want %d", len(fields), len(labels))
+// 			}
 
-			for i := 0; i < len(fields); i++ {
-				if fields[i].Label != labels[i] {
-					t.Errorf("fields()[%d].Label = %s; want %s", i, fields[i].Label, labels[i])
-				}
-			}
-		}
-	}
+// 			for i := 0; i < len(fields); i++ {
+// 				if fields[i].Label != labels[i] {
+// 					t.Errorf("fields()[%d].Label = %s; want %s", i, fields[i].Label, labels[i])
+// 				}
+// 			}
+// 		}
+// 	}
 
-	hasValues := func(values ...interface{}) func(*testing.T, []field) {
-		return func(t *testing.T, fields []field) {
-			if len(fields) != len(values) {
-				t.Fatalf("fields() len = %d; want %d", len(fields), len(values))
-			}
+// 	hasValues := func(values ...interface{}) func(*testing.T, []field) {
+// 		return func(t *testing.T, fields []field) {
+// 			if len(fields) != len(values) {
+// 				t.Fatalf("fields() len = %d; want %d", len(fields), len(values))
+// 			}
 
-			for i := 0; i < len(fields); i++ {
-				if fields[i].Value != values[i] {
-					t.Errorf("fields()[%d].Value = %v; want %s", i, fields[i].Label, values[i])
-				}
-			}
-		}
-	}
+// 			for i := 0; i < len(fields); i++ {
+// 				if fields[i].Value != values[i] {
+// 					t.Errorf("fields()[%d].Value = %v; want %s", i, fields[i].Label, values[i])
+// 				}
+// 			}
+// 		}
+// 	}
 
-	check := func(checks ...func(*testing.T, []field)) []func(*testing.T, []field) {
-		return checks
-	}
+// 	check := func(checks ...func(*testing.T, []field)) []func(*testing.T, []field) {
+// 		return checks
+// 	}
 
-	tests := map[string]struct {
-		strct  interface{}
-		checks []func(*testing.T, []field)
-	}{
-		"No Values": {
-			// value in field
-			strct: struct {
-				Name string
-			}{},
-			checks: check(hasLabels("Name")),
-		},
-		"Multiple fields with values": {
-			// value in field
-			strct: struct {
-				Name  string
-				Email string
-				Age   int
-			}{
-				Name:  "Jon Calhoun",
-				Email: "jon@calhoun.io",
-				Age:   123,
-			},
-			checks: check(
-				hasLabels("Name", "Email", "Age"),
-				hasValues("Jon Calhoun", "jon@calhoun.io", 123),
-			),
-		},
-	}
-	for name, tc := range tests {
+// 	tests := map[string]struct {
+// 		strct  interface{}
+// 		checks []func(*testing.T, []field)
+// 	}{
+// 		"No Values": {
+// 			// value in field
+// 			strct: struct {
+// 				Name string
+// 			}{},
+// 			checks: check(hasLabels("Name")),
+// 		},
+// 		"Multiple fields with values": {
+// 			// value in field
+// 			strct: struct {
+// 				Name  string
+// 				Email string
+// 				Age   int
+// 			}{
+// 				Name:  "Jon Calhoun",
+// 				Email: "jon@calhoun.io",
+// 				Age:   123,
+// 			},
+// 			checks: check(
+// 				hasLabels("Name", "Email", "Age"),
+// 				hasValues("Jon Calhoun", "jon@calhoun.io", 123),
+// 			),
+// 		},
+// 	}
+// 	for name, tc := range tests {
 
-		t.Run(name, func(t *testing.T) {
-			got := fields(tc.strct)
+// 		t.Run(name, func(t *testing.T) {
+// 			got := fields(tc.strct)
 
-			for _, check := range tc.checks {
-				check(t, got)
-			}
-		})
-	}
-}
+// 			for _, check := range tc.checks {
+// 				check(t, got)
+// 			}
+// 		})
+// 	}
+// }
