1469c4c71be09e62b22671659c4958f2732be941
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-02-28 21:33:44 +0700

    first test

---
+++ project-2-stripe/client_test.go
@@ -0,0 +1,22 @@
+package stripe_test
+
+import (
+	"strings"
+	stripe "test-with-go/project-2-stripe"
+	"testing"
+)
+
+func TestClient_Customer(t *testing.T) {
+	c := stripe.Client{}
+	tok := "tox_amex"
+	cus, err := c.Customer(tok)
+	if err != nil {
+		t.Errorf("Customer() err = %s; want %v", err, nil)
+	}
+	if cus == nil {
+		t.Fatalf("Customer() = nil; want non-nil value")
+	}
+	if strings.HasPrefix(cus.ID, "cus_") {
+		t.Errorf("Customer() ID = %s; want prefix %q", cus.ID, "cus_")
+	}
+}
