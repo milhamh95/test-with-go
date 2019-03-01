2f85bf3801faca442ea631652c0a00efa9ed4b3f
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-03-01 10:44:43 +0700

    creating a customer

--- project-2-stripe/client_test.go
+++ project-2-stripe/client_test.go
@@ -7,16 +7,18 @@ import (
 )
 
 func TestClient_Customer(t *testing.T) {
-	c := stripe.Client{}
-	tok := "tox_amex"
+	c := stripe.Client{
+		Key: "sk_test_4eC39HqLyjWDarjtT1zdp7dc",
+	}
+	tok := "tok_amex"
 	cus, err := c.Customer(tok)
 	if err != nil {
-		t.Errorf("Customer() err = %s; want %v", err, nil)
+		t.Errorf("Customer() err = %v; want %v", err, nil)
 	}
 	if cus == nil {
 		t.Fatalf("Customer() = nil; want non-nil value")
 	}
-	if strings.HasPrefix(cus.ID, "cus_") {
+	if !strings.HasPrefix(cus.ID, "cus_") {
 		t.Errorf("Customer() ID = %s; want prefix %q", cus.ID, "cus_")
 	}
 }
