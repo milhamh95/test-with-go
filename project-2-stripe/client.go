53f0f71acfdeadb08509f9c2fc9537503f9de9d1
M Ilham Hidayat <m.ilham.hidayat.95@gmail.com>
2019-03-01 15:54:06 +0700

    the charge endpoint

--- project-2-stripe/client.go
+++ project-2-stripe/client.go
@@ -6,17 +6,28 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
 )
 
 const (
-	Version = "2019-02-19"
+	Version         = "2019-02-19"
+	DefaultCurrency = "usd"
 )
 
 type Customer struct {
 	ID            string `json:"id"`
 	DefaultSource string `json:"default_source"`
 	Email         string `json:"email"`
+}
+
+type Charge struct {
+	ID             string `json:"id"`
+	Amount         int    `json:"amount"`
+	FailureCode    string `json:"failure_code"`
+	FailureMessage string `json:"failure_message"`
+	Paid           bool   `json:"paid"`
+	Status         string `json:"status"`
 }
 
 type Client struct {
@@ -56,4 +67,40 @@ func (c *Client) Customer(token string, email string) (*Customer, error) {
 	}
 
 	return &cus, nil
+}
+
+func (c *Client) Charge(customerID string, amount int) (*Charge, error) {
+	endpoint := "https://api.stripe.com/v1/charges"
+	v := url.Values{}
+	v.Set("customer", customerID)
+	v.Set("amount", strconv.Itoa(amount))
+	v.Set("currency", DefaultCurrency)
+	req, err := http.NewRequest(http.MethodPost, endpoint, strings.NewReader(v.Encode()))
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Stripe-Version", Version)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(c.Key, "")
+
+	httpClient := http.Client{}
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(string(body))
+
+	var chg Charge
+	err = json.Unmarshal(body, &chg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &chg, nil
 }
--- project-2-stripe/client_test.go
+++ project-2-stripe/client_test.go
@@ -40,3 +40,34 @@ func TestClient_Customer(t *testing.T) {
 		t.Errorf("Customer() ID = %s; want prefix %q", cus.DefaultSource, "card_")
 	}
 }
+
+func TestClient_Charge(t *testing.T) {
+
+	if apiKey == "" {
+		t.Skip("No API key provided")
+	}
+
+	c := stripe.Client{
+		Key: apiKey,
+	}
+	tok := "tok_amex"
+	email := "test@testwithgo.com"
+	cus, err := c.Customer(tok, email)
+	if err != nil {
+		t.Errorf("Customer() err = %v; want %v", err, nil)
+	}
+
+	// Create a customer for the test
+	cusID := cus.ID
+	amount := 1234
+	charge, err := c.Charge(cusID, amount)
+	if err != nil {
+		t.Errorf("Charge() err = %v; want %v", err, nil)
+	}
+	if charge == nil {
+		t.Fatalf("Charge() = nil; want non-nil value")
+	}
+	if charge.Amount != amount {
+		t.Errorf("Charge() .amount = %d; want %d", charge.Amount, amount)
+	}
+}
